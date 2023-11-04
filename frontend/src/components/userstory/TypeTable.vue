2da36aef9b784cb1ad799bab24434dcdf1bc0f7f
Lucas Vieira <lucasvieiradasilva07@gmail.com>
2023-11-04 20:31:34 -0300

    feat: criado tabela de tipo hu e task

--- frontend/src/components/userstory/TypeTable.vue
+++ frontend/src/components/userstory/TypeTable.vue
@@ -2,6 +2,7 @@
 import { ref, onBeforeMount, watch } from 'vue'
 import { VDataTable } from 'vuetify/labs/VDataTable'
 import TypeCard from '@/components/userstory/TypeCard.vue'
+import TaskTypeTable from '@/components/task/TypeTable.vue'
 import api from '@api'
 
 
@@ -23,19 +24,128 @@ const headers = [
 	key: 'description'
     },
     {
+	title: 'Tipo de Epico',
+	sortable: true,
+	key: 'epicType.description'
+    },
+    {
+	title: 'Tipos de Tarefa',
+	sortable: false,
+	align: 'center',
+	key: 'tasktypes'
+    },
+    {
+	title: 'Historias de Usuario',
+	sortable: false,
+	align: 'center',
+	key: 'userstories'
+    },
+    {
 	title: 'Acoes',
 	key: 'actions'
     },
 ]
-const items = ref([])
+const items = ref([
+{
+  "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "description": "tipo de historia de usuario",
+  "epicType": {
+    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "description": "tipo de epico",
+    "userStoryTypeList": [
+      "string"
+    ],
+    "epics": [
+      {
+        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "description": "string",
+        "title": "string",
+        "relevance": 0,
+        "category": "CATEGORY",
+        "userStories": [
+          "string"
+        ],
+        "epicType": "string",
+        "project": {
+          "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+          "name": "string",
+          "epics": [
+            "string"
+          ]
+        },
+        "depends": [
+          "string"
+        ]
+      }
+    ]
+  },
+  "taskTypeList": [
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "description": "string",
+      "userStoryType": "string"
+    }
+  ],
+  "userStories": [
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "title": "string",
+      "description": "string",
+      "relevance": 0,
+      "category": "CATEGORY",
+      "tasks": [
+        {
+          "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+          "description": "string",
+          "title": "string",
+          "userStory": "string",
+          "taskType": {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "description": "string",
+            "userStoryType": "string"
+          },
+          "depends": [
+            "string"
+          ]
+        }
+      ],
+      "epic": {
+        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "description": "string",
+        "title": "string",
+        "relevance": 0,
+        "category": "CATEGORY",
+        "userStories": [
+          "string"
+        ],
+        "epicType": "string",
+        "project": {
+          "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+          "name": "string",
+          "epics": [
+            "string"
+          ]
+        },
+        "depends": [
+          "string"
+        ]
+      },
+      "userStoryType": "string",
+      "depends": [
+        "string"
+      ]
+    }
+  ]
+}
+])
 
 async function fetchItems() {
-    const { data } = await api.get('/epicType')
+    const { data } = await api.get('/userStoryType')
     items.value = data
 }
 
 async function deleteItem(id) {
-    const { data } = await api.delete(`/epicType/${id}/`)
+    const { data } = await api.delete(`/userStoryType/${id}/`)
     console.log(data)
     await fetchItems()
 }
@@ -62,18 +172,53 @@ async function onUpdate() {
     await fetchItems()
 }
 
+
+
+const taskTypesModal = ref(false)
+const taskTypes = ref([])
+function showTaskTypes(newTaskTypes) {
+    taskTypes.value = newTaskTypes
+    taskTypesModal.value = true
+}
+function hideTaskTypes() {
+    taskTypesModal.value = false
+    taskTypes.value = []
+}
+
+
+const userStoriesModal = ref(false)
+const userStories = ref([])
+function showUserStories(newUsrStories) {
+    userStories.value = newUsrStories
+    userStoriesModal.value = true
+}
+function hideUserStories() {
+    userStories.value = []
+    userStoriesModal.value = false
+}
+
+
+
 watch(() => props.refresh, fetchItems)
 
-onBeforeMount(fetchItems)
+// onBeforeMount(fetchItems)
 </script>
 
 <template>
     <v-card>
 	<v-card-item>
-	    <v-card-title>{{ props.selectMode ? 'Selecionar Tipo de Epico' : 'Tipos de Epico' }}</v-card-title>
+	    <v-card-title>
+		{{ props.selectMode ? 'Selecionar Tipo de Historia de Usuario' : 'Tipos de Historia de Usuario' }}
+	    </v-card-title>
 	</v-card-item>
 	<v-card-text>
 	    <v-data-table :headers='headers' :items='items' density='compact'>
+		<template #item.tasktypes="{ item }">
+		    <v-btn @click='showTaskTypes(item.taskTypeList)'>Ver</v-btn>
+		</template>
+		<template #item.userstories="{ item }">
+		    <v-btn @click='showUserStories(item.userStories)'>Ver</v-btn>
+		</template>
 		<template #item.actions="{ item }">
 		    <v-btn color='black' icon @click='deleteItem(item.id)'>
 			<v-tooltip activator='parent' location='top'>Excluir</v-tooltip>
@@ -99,6 +244,9 @@ onBeforeMount(fetchItems)
 	</v-dialog>
 	<v-dialog v-model='createModal'>
 	    <type-card mode='create' @create='onCreate' />
+	</v-dialog>
+	<v-dialog v-model='taskTypesModal'>
+	    <task-type-table :modelValue='taskTypes' />
 	</v-dialog>
     </v-card>
 </template>
--- frontend/src/views/UserStory.vue
+++ frontend/src/views/UserStory.vue
@@ -1,7 +1,9 @@
 <script setup>
 import TypeCard from '@/components/userstory/TypeCard.vue'
+import TypeTable from '@/components/userstory/TypeTable.vue'
 </script>
 
 <template>
     <type-card mode='create' @create='console.log' />
+    <type-table />
 </template>
